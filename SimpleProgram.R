e9febe5037aac9d3db2a856edf78782dda82187e
Ananya RoyChowdhury <ananya.rc94@tamu.edu>
2019-08-30 21:57:52 -0500

    Completed SimpleProgram.R with functions

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -8,8 +8,14 @@ sigma = 2 # noise standard deviation
 beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
-# [ToDo] Use generateY function to generate Y
+# Function to generate Y
 
-# [ToDo] Use calculateBeta function to calculate beta_LS
+Y = generateY(X,beta,sigma,seed=5832652)
 
-# [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
+# Function to calculate beta_LS
+
+beta_LS = calculateBeta(X,Y)
+
+# Function to calculate MSE to assess the estimation error
+
+MSE = calculateMSE(beta,beta_LS)
